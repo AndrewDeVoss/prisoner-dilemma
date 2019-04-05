ec5530f23ac00fb6dbc2d38d85808fe225dff718
Andrew <apdevoss99@gmail.com>
2019-04-04 22:10:08 -0500

    Now the assignScore method actually works, I continued the evolve Population method. Next should be re-sorting the population based on each delinquent's score and actually evolving the population (within the evolvePopulation method). TL;DR The assignScore method is working

--- PrisonerDilemna.py
+++ PrisonerDilemna.py
@@ -7,6 +7,8 @@ class Delinquent():
         self.choice = biteString[len(biteString) - 1]
         self.characteristics = biteString[:(len(biteString) - 1)]
         self.score = 0
+        self.opponentList = []
+
     def getBiteString(self):
         return self.biteString
     def getChoice(self):
@@ -15,6 +17,7 @@ class Delinquent():
         return self.characteristics
     def getScore(self):
         return self.score
+
     def __str__(self):
         return str(self.biteString)
     def __gt__(self, otherDelinquent):
@@ -47,6 +50,13 @@ def evolvePopulation(desiredPopulationSize, lenBiteString, population=None):
         x = Delinquent(makeBiteString(lenBiteString))
         population.append(x)
     newPop = mergeSortPopulation(population, lenBiteString)
+    left = 0
+    right = 0
+    while right < len(newPop):
+        while right < len(newPop) and newPop[right].biteString == newPop[left].biteString:
+            right = right + 1
+        assignScores(newPop[left:right], newPop)
+        left = right
 
     return newPop
 
@@ -78,7 +88,7 @@ def mergeSortPopulation(population, numCharacteristics):
     else:
         return copy.deepcopy(population)
 
-def assignScores(partialPopulation, population=None):
+def assignScores(partialPopulation, population):
     # The last byte in byteString represents the delinquent's decision
     # 1 represents cooperative
     # 0 represents defector
@@ -87,9 +97,11 @@ def assignScores(partialPopulation, population=None):
         delinquent = partialPopulation[0]
         opponent = partialPopulation[0]
         while opponent == partialPopulation[0]:
-            opponent = population[random.randint(0, len(population))]
+            opponent = population[random.randint(0, len(population)-1)]
         delDecision = delinquent.biteString[len(delinquent.biteString) - 1]
         opDecision = opponent.biteString[len(opponent.biteString) - 1]
+        # delinquent.opponentList.append(opponent.biteString)
+        # opponent.opponentList.append(delinquent.biteString)
         if delDecision == 1:
             if opDecision == 1:
                 delinquent.score = delinquent.score + 3
@@ -113,6 +125,8 @@ def assignScores(partialPopulation, population=None):
             for jndex in range(index + 1, len(partialPopulation)):
                 opponent = partialPopulation[jndex]
                 opDecision = opponent.biteString[len(opponent.biteString) - 1]
+                # delinquent.opponentList.append(opponent.biteString)
+                # opponent.opponentList.append(delinquent.biteString)
                 if delDecision == 1:
                     if opDecision == 1:
                         delinquent.score = delinquent.score + 3
@@ -130,10 +144,19 @@ def assignScores(partialPopulation, population=None):
 
 
 def main():
+    # used to fix assignScore and test the evolvePop
+    """
+    population = evolvePopulation(20, 3)
+    for delinquent in population:
+        print("biteString, score, opponentList: ", delinquent.biteString, delinquent.score, delinquent.opponentList)
+    """
+    # used to test assignScores (modified so that population is initialized as None)
+    """
     population = evolvePopulation(4, 2)
     assignScores(population)
     for i in range(0, len(population)):
         print(population[i], population[i].getScore())
+    """
     # used to test mergeSortPop
     """
     population = evolvePopulation(10, 6)
